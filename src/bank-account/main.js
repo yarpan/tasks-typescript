bcc5a994ee4ef8695bd455bf756392d40c6e9270
Yaroslav Panchishyn <yarpan@gmail.com>
2025-01-20 12:44:06 +0200

    refactoring

--- src/bank-account/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const bank_account_1 = require("./bank-account");
-const myBankAccount = new bank_account_1.BankAccount(100);
-myBankAccount.getBalance();
-myBankAccount.deposit(90);
-myBankAccount.withdraw(-60);
-myBankAccount.withdraw(60);
-//# sourceMappingURL=main.js.map
