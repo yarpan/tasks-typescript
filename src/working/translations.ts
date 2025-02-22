14f5291e219728560d719a9f60c5fe96de5cf6d1
Yaroslav Panchishyn <yarpan@gmail.com>
2025-02-22 23:59:13 +0200

    add task

--- src/working/translations.ts
+++ src/working/translations.ts
@@ -2,21 +2,13 @@
 /*
 Task: Translations Type
 Create a type Translations that describes an object with string keys, where:
-
 Each key is a language code (e.g., en, ua), and the value is the translation.
-
 Use an index signature to indicate that keys can be any string.
-
 Allow for some languages to have missing translations (optional values).
-
 Create an object appTranslations that conforms to the Translations type and contains translations of several phrases for different languages.
-
 Dynamically access keys (e.g., through appTranslations[someVariable]) and verify how type annotation works.
-
 Try accessing a non-existent key and ensure that the compiler warns about the possibility of undefined.
-
 Add another type, for example, OptionalTranslations with an optional field, and try to align it with Translations to check the compatibility of index signatures with optional fields.
-
 For example, create a type with a default? field.
 */
 
--- src/working/type-assertions.ts
+++ src/working/type-assertions.ts
@@ -1,15 +1,10 @@
 /*
 Task: Create the fetchData Method
 Create a method fetchData that returns a value of type unknown.
-
 Assume that this function returns an object with fields name: string and age: number, but the return type is not known.
-
 Use different type assertion methods to cast the result of fetchData to the expected object type.
-
 Save the obtained result in a variable with a refined type.
-
 Add a method printPersonInfo, in whose signature use Signature Assertion to ensure that the parameter person is an object with fields name: string and age: number.
-
 Invoke fetchData and pass the result to printPersonInfo to ensure correct operation.
 */
 
